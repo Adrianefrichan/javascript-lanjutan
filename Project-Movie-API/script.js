71092316893094a6b851298405eee55f9ccef0ba
Adrian <adrianefrich@gmail.com>
2025-08-22 10:10:19 +0700

    perubahan dari jquery ke vanila javascript menggunakan fetch

--- Project-Movie-API/script.js
+++ Project-Movie-API/script.js
@@ -1,36 +1,73 @@
-$('.search-button').on('click', function(){
-    $.ajax({
-        url: 'http://www.omdbapi.com/?apikey=651ca36f&s=' + $('.input-keyword').val(),
-        success: results => {
-            const movies = results.Search;
-            console.log(movies);
-            let cards = ''; 
-            movies.forEach(m => {
-                cards += showCards(m);
-            });
-            $('.movie-container').html(cards);
+// jQuery
+// $('.search-button').on('click', function(){
+//     $.ajax({
+//         url: 'http://www.omdbapi.com/?apikey=651ca36f&s=' + $('.input-keyword').val(),
+//         success: results => {
+//             const movies = results.Search;
+//             console.log(movies);
+//             let cards = ''; 
+//             movies.forEach(m => {
+//                 cards += showCards(m);
+//             });
+//             $('.movie-container').html(cards);
     
     
-            // Ketika tombol detail  di klik
-            $('.modal-detail-button').on('click', function(){
-                $.ajax({
-                    url :  'http://www.omdbapi.com/?apikey=651ca36f&i=' + $(this).data('imdbid'),
-                    success : m => {
-                        const movieDetail = showDetail(m);
-                    $('.modal-body').html(movieDetail);
-                    },
-                    error:(e) => {
-                        console.log(e.responText);
-        }
-                })
+//             // Ketika tombol detail  di klik
+//             $('.modal-detail-button').on('click', function(){
+//                 $.ajax({
+//                     url :  'http://www.omdbapi.com/?apikey=651ca36f&i=' + $(this).data('imdbid'),
+//                     success : m => {
+//                         const movieDetail = showDetail(m);
+//                     $('.modal-body').html(movieDetail);
+//                     },
+//                     error:(e) => {
+//                         console.log(e.responText);
+//         }
+//                 })
+//             });
+//         },
+//         error:(e) => {
+//             console.log(e.responText);
+//         }
+//     });
+
+// });
+
+
+
+// fetch
+
+// ambil data
+const searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', function(){
+    const inputKeyword = document.querySelector('.input-keyword');
+    fetch('http://www.omdbapi.com/?apikey=651ca36f&s=' + inputKeyword.value)
+        .then(response => response.json())
+        .then(response => {
+            const movie = response.Search;
+            let cards = '';
+            movie.forEach(m => cards += showCards(m));
+            const movieContainer = document.querySelector('.movie-container');
+            movieContainer.innerHTML = cards;
+
+             // ketika tombol di click
+        const modalDetailButton = document.querySelectorAll('.modal-detail-button');
+        modalDetailButton.forEach(btn => {
+        btn.addEventListener('click',function(){
+            const imdbid = this.dataset.imdbid;
+            fetch('http://www.omdbapi.com/?apikey=651ca36f&i=' + imdbid)
+            .then(response => response.json())
+            .then( m => {
+                const movieDetail = showDetail(m);
+                const modalBody = document.querySelector('.modal-body');
+                modalBody.innerHTML = movieDetail;
             });
-        },
-        error:(e) => {
-            console.log(e.responText);
-        }
+        });
     });
+        });
 
 });
+
 
 
 
@@ -41,10 +78,10 @@ function showCards(m){
                     <div class="card-body">
                         <h5 class="card-title">${m.Title}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
-                        <a href="#" class="btn btn-primary modal-detail-button"  data-bs-toggle="modal" data-bs-target="#MovieDetailModal" data-imdbid = ${m.imdbID}>Show Details</a>
+                        <a href="#" class="btn btn-primary modal-detail-button"  data-bs-toggle="modal" data-bs-target="#MovieDetailModal" data-imdbid = "${m.imdbID}">Show Details</a>
                     </div>
                     </div>
-            </div>`
+            </div>`;
 }
 
 
@@ -52,11 +89,11 @@ function showDetail(m){
             return  `<div class="container-fluid">
                             <div class="row">
                                 <div class="col-md-3">
-                                <img src=${m.Poster} class="img-fluid">
+                                <img src="${m.Poster}" class="img-fluid">
                                 </div>
                                 <div class="col-md">
                                 <ul class="list-group">
-                                    <li class="list-group-item"><h4>${m.title}</h4></li>
+                                    <li class="list-group-item"><h4>${m.Title}</h4></li>
                                     <li class="list-group-item"><strong>Director :</strong> ${m.Director}</li>
                                     <li class="list-group-item"><strong>Actors :</strong> ${m.Actors}</li>
                                     <li class="list-group-item"><strong>Writer :</strong> ${m.Writer}</li>
@@ -64,5 +101,5 @@ function showDetail(m){
                                 </ul>
                                 </div>
                             </div>
-                            </div>`
+                            </div>`;
 }
